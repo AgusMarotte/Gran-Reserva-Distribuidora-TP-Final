2302ca953b98a6ff207cfc276f000ec49003c0cb
Victoria Fabi <victoria.fabi87@gmail.com>
2025-10-11 10:55:38 -0300

    Actualizar clase User

--- Backend/src/Domain/Entities/User.cs
+++ Backend/src/Domain/Entities/User.cs
@@ -2,11 +2,33 @@ namespace Domain.Entities
 {
     public abstract class User
     {
-        public int Id { get; set; }
-        public string ClientName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public int Id { get; private set; }
+        public string ClientName { get; private set; }
+        public string LastName { get; private set; } 
+        public string Email { get; private set; } 
+        public string Password { get; private set; }
+
+        protected User() { }
+
+        protected User(string clientName, string lastName, string email, string password)
+        {
+            ClientName = clientName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+        }
+
+        public void SetPassword(string newPassword)
+        {
+            Password = newPassword;
+        }
+        
+        public void UpdateInfo(string clientName, string lastName, string email)
+        {
+            ClientName = clientName;
+            LastName = lastName;
+            Email = email;
+        }
 
     }
 
